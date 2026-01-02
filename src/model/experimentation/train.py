b06705ba35ed59c0f03a0b9b5649180577f2e6e8
ammanjunath <manjunathtek@gmail.com>
2026-01-02 14:39:37 +0530

    Remove unused import for mean_squared_error

--- src/model/experimentation/train.py
+++ src/model/experimentation/train.py
@@ -1,6 +1,5 @@
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import joblib
