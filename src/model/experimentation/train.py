cb501ab7080c8f1b2ed34ac82b2ff30095a01951
ammanjunath <manjunathtek@gmail.com>
2026-01-02 14:20:10 +0530

    Comment out model metrics retrieval
    Comment out the line that retrieves model metrics.

--- src/model/experimentation/train.py
+++ src/model/experimentation/train.py
@@ -52,7 +52,7 @@ def main():
     reg = train_model(data, args)
 
     # Validate Model on Validation Set
-    metrics = get_model_metrics(reg, data)
+    # metrics = get_model_metrics(reg, data)
 
     # Save Model
     model_name = "sklearn_regression_model.pkl"
