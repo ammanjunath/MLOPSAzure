98c80ec135a6fdbc19c2deebe3186f9430be605c
ammanjunath <manjunathtek@gmail.com>
2026-01-02 16:14:05 +0530

    Update score.py

--- experimentation/score.py
+++ experimentation/score.py
@@ -19,7 +19,10 @@ def run(raw_data, request_headers):
 
 if __name__ == "__main__":
     init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
+    test_row = (
+        '{"data":[[1,2,3,4,5,6,7,8,9,10],
+        [10,9,8,7,6,5,4,3,2,1]]}'
+    )
     request_header = {}
     prediction = run(test_row, request_header)
     print("Test result:", prediction)
