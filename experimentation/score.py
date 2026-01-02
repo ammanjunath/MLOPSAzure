49d134b4aac57d94c50039aecaaa6529c17e912c
ammanjunath <manjunathtek@gmail.com>
2026-01-02 15:40:16 +0530

    Reformat score.py with consistent spacing

--- experimentation/score.py
+++ experimentation/score.py
@@ -4,11 +4,15 @@ from azureml.core.model import Model
 import joblib
 
 
+
+
 def init():
     """ This is an init model for sklearn """
     global model
     model_path = Model.get_model_path(model_name="sklearn_regression_model.pkl")
     model = joblib.load(model_path)
+
+
 
 
 def run(raw_data, request_headers):
@@ -26,5 +30,6 @@ if __name__ == "__main__":
     prediction = run(test_row, request_header)
 
     print("Test result:", prediction)
+
 
 
