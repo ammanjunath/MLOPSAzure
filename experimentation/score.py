6741b9ced5a84c7607270a80f172ec41c2da04cf
ammanjunath <manjunathtek@gmail.com>
2026-01-02 15:13:09 +0530

    Update score.py

--- experimentation/score.py
+++ experimentation/score.py
@@ -2,14 +2,11 @@ import json
 import numpy
 from azureml.core.model import Model
 import joblib
-
 def init():
     global model
     model_path = Model.get_model_path(
         model_name="sklearn_regression_model.pkl")
     model = joblib.load(model_path)
-
-
 def run(raw_data, request_headers):
     data = json.loads(raw_data)["data"]
     data = numpy.array(data)
